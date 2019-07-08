0cc0c671f28d45e55d8ffa22aa02ec2e7ac4ff16
fernandaWin <fernand4.reyes@gmail.com>
2019-07-08 18:13:44 -0400

    hasta cap 26

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Empleado} from './Empleado';
+import { timingSafeEqual } from 'crypto';
 
 
 @Component ({
@@ -8,18 +9,26 @@ import {Empleado} from './Empleado';
 })
 
 export class EmpleadoComponent{
+
     public tittle_empleado = 'Componente empleado';
     public empleado : Empleado;
     public trabajadores :Array <Empleado>;
+    public trabajador_externo:boolean;
+    public color:string;
+
 
     constructor(){
 
         this.empleado = new Empleado('Boris', 23, 'cocinero', true);
+
         this.trabajadores = [
-            new Empleado('Boris', 23, 'cocinero', true),
-            new Empleado('Boris2', 24, 'cocinero2', false),
-            new Empleado('Boris3', 25, 'cocinero3', true)
+            new Empleado('Boris', 23, 'cocinero array 0', true),
+            new Empleado('Boris2', 24, 'cocinero2 array 1', false),
+            new Empleado('Boris3', 25, 'cocinero3 array 2', true)
         ];
+
+        this.trabajador_externo = false;
+        this.color = 'green';
 
     }
 
@@ -28,4 +37,10 @@ export class EmpleadoComponent{
         console.log(this.empleado);
         console.log(this.trabajadores);
     }
+
+    cambiarExterno(valor){
+        this.trabajador_externo = valor;
+
+    }
+
 }
